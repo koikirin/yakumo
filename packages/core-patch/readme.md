d7b1a0d913eb0d106c41aade76d72e8d94e7d046
Hieuzest <girkirin@hotmail.com>
2023-09-01 22:56:00 +0800

    feat(core-patch): add plugin core-patch

---
+++ packages/core-patch/readme.md
@@ -0,0 +1,7 @@
+# yakumo-patch
+
+[![downloads](https://img.shields.io/npm/dm/yakumo-core-patch?style=flat-square)](https://www.npmjs.com/package/yakumo-core-patch)
+[![npm](https://img.shields.io/npm/v/yakumo-core-patch?style=flat-square)](https://www.npmjs.com/package/yakumo-core-patch)
+[![GitHub](https://img.shields.io/github/license/Hieuzest/yakumo?style=flat-square)](https://github.com/Hieuzest/yakumo/blob/master/LICENSE)
+
+Yakumo core patch.
---
+++ packages/core-patch/src/index.ts
@@ -0,0 +1,44 @@
+import { Awaitable, pick, Promisify } from 'cosmokit'
+import { commands, hooks, Options, Project } from 'yakumo'
+
+declare module 'yakumo' {
+  export interface Project {
+    emit<T extends keyof Hooks>(name: T, ...args: Parameters<Hooks[T]>): Promise<void>
+    serial<T extends keyof Hooks>(name: T, ...args: Parameters<Hooks[T]>): Promisify<ReturnType<Hooks[T]>>
+  }
+
+  export interface Hooks {
+    'execute.prepare'?: (project: Project, name: string) => Awaitable<true | void>
+    'execute.before'?: (project: Project, name: string) => Awaitable<void>
+    'execute.after'?: (project: Project, name: string) => Awaitable<void>
+  }
+}
+
+Project.prototype.serial = async (name: string, ...args: any) => {
+  for (const callback of (hooks[name] || [])) {
+    const result = await callback.call(this, ...args)
+    if (result) return result
+  }
+}
+
+function setTargets(project: Project) {
+  project.targets = pick(project.workspaces, project.argv._.flatMap((name: string) => {
+    return project.locate(name)
+  }))
+}
+
+export function register(name: string, callback: (project: Project) => void, options: Options = {}) {
+  const manual = options.manual
+  commands[name] = [async (project) => {
+    project.argv.config.manual = manual
+    if (!await project.serial('execute.prepare', project, name)) {
+      setTargets(project)
+    }
+    await project.serial('execute.before', project, name)
+    const ret = callback(project)
+    await project.emit('execute.after', project, name)
+    return ret
+  }, { ...options, manual: true }]
+}
+
+require('yakumo').register = register
