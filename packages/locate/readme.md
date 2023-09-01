63f344eb098952522b373057c827a4af478567da
Hieuzest <girkirin@hotmail.com>
2023-09-01 22:56:41 +0800

    feat(locate): add plugin locate

---
+++ packages/locate/readme.md
@@ -0,0 +1,7 @@
+# yakumo-locate
+
+[![downloads](https://img.shields.io/npm/dm/yakumo-locate?style=flat-square)](https://www.npmjs.com/package/yakumo-locate)
+[![npm](https://img.shields.io/npm/v/yakumo-locate?style=flat-square)](https://www.npmjs.com/package/yakumo-locate)
+[![GitHub](https://img.shields.io/github/license/Hieuzest/yakumo?style=flat-square)](https://github.com/Hieuzest/yakumo/blob/master/LICENSE)
+
+Locate workspaces flexibly.
---
+++ packages/locate/src/index.ts
@@ -0,0 +1,104 @@
+import { cyan, green } from 'kleur'
+import { addHook, Project } from 'yakumo'
+import { Awaitable, difference, makeArray, pick } from 'cosmokit'
+import {} from 'yakumo-core-patch'
+
+declare module 'yakumo' {
+  export interface Hooks {
+    'locate.trigger'?: (project: Project, name: string, options?: LocateOptions) => Awaitable<void>
+  }
+
+  export interface Commands {
+    [key: string]: LocateConfig
+  }
+
+  export interface Arguments {
+    locate: LocateConfig | false
+  }
+}
+
+export interface LocateConfig {
+  'exclude-packages': string[]
+}
+
+export interface LocateOptions {
+  root?: boolean
+  folder?: boolean
+  package?: boolean
+}
+
+const buildFnmatch = (glob) => {
+  const matcher = glob
+    .replace(/\*/g, '.*')
+    .replace(/\?/g, '.')
+  const r = new RegExp(`^${matcher}$`)
+  return r
+}
+
+function locateViaFolder(project: Project, name: string, options: LocateOptions = {}) {
+  if (project.config.alias[name]) {
+    return makeArray(project.config.alias[name]).map((path) => {
+      if (!project.workspaces[path]) {
+        throw new Error(`cannot find workspace ${path} resolved by ${name}`)
+      }
+      return path
+    })
+  }
+
+  const targets = Object.keys(project.workspaces).filter((folder) => {
+    const matcher = buildFnmatch(name)
+    const json = project.workspaces[folder]
+    if (!options.root && json.workspaces) return
+    const [last] = folder.split('/').reverse()
+    return name === last || matcher.test(folder) || matcher.test(folder && folder.slice(1))
+  })
+  return targets
+}
+
+function locateViaPackage(project: Project, name: string, options: LocateOptions = {}) {
+  const matcher = buildFnmatch(name)
+  const targets = Object.keys(project.workspaces).filter((folder) => {
+    const json = project.workspaces[folder]
+    if (!options.root && json.workspaces) return
+    const [last] = json.name.split('/').reverse()
+    return (name.startsWith('!') && name.slice(1) === json.name) || matcher.test(json.name) || matcher.test(last)
+  })
+  return targets
+}
+
+function locate(project: Project, name: string, options: LocateOptions = {}) {
+  return [
+    ...options.folder ? locateViaFolder(project, name, options) : [],
+    ...options.package ? locateViaPackage(project, name, options) : [],
+  ]
+}
+
+async function setTargets(project: Project, name: string, options: LocateOptions = {}) {
+  const o = { root: false, folder: true, package: true, ...project.argv.locate || {}, ...options }
+  const excludes = project.config.commands?.[name]?.['exclude-packages']?.flatMap(name => locate(project, name, o)) || []
+  const includes = project.argv._.flatMap((name: string) => locate(project, name, o))
+
+  project.targets = pick(project.workspaces, difference(includes, excludes))
+
+  console.log(cyan(`[${name}]`), green(`Located ${Object.keys(project.targets).length} workspaces`))
+}
+
+addHook('execute.prepare', (project, name) => true)
+
+addHook('execute.before', (project, name) => {
+  if (!project.argv._.length || project.argv.config.manual) {
+    project.targets = { ...project.workspaces }
+    return
+  }
+
+  if (project.argv.locate === false && !project.targets?.length) {
+    project.targets = pick(project.workspaces, project.argv._.flatMap((name: string) => {
+      return project.locate(name)
+    }))
+    return
+  }
+
+  setTargets(project, name)
+})
+
+addHook('locate.trigger', setTargets)
