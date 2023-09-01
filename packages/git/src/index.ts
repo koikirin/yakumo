837a43e0c1351db0be0fe6fdcf1ea2ab15c18e50
Hieuzest <girkirin@hotmail.com>
2023-09-02 03:02:56 +0800

    feat(git): support non-root packages

--- packages/git/src/index.ts
+++ packages/git/src/index.ts
@@ -1,59 +1,100 @@
 import { CheckRepoActions, SimpleGit, simpleGit } from 'simple-git'
 import { cyan, green, red, yellow } from 'kleur'
 import { Project, register } from 'yakumo'
+import { isAbsolute, relative, resolve } from 'path'
 import {} from 'yakumo-core-patch'
 import {} from 'yakumo-locate'
+
+declare module 'yakumo' {
+  interface Arguments {
+    dry?: boolean
+    root?: boolean
+    workingDirectories?: string
+    message: string
+    remote?: string
+    branch?: string
+  }
+}
+
+function isSubdirectoryOf(dir: string, parent: string) {
+  const relpath = relative(parent, dir)
+  return relpath && !relpath.startsWith('..') && !isAbsolute(relpath)
+}
 
 async function status(project: Project, name: string) {
   try {
     if (!name) return false
     const git: SimpleGit = simpleGit(name.slice(1))
-    if (!await git.checkIsRepo(CheckRepoActions.IS_REPO_ROOT)
-    ) return false
-    const s = await git.status()
-    console.log(cyan(name), s.files.map(f => `${yellow(f.working_dir)} ${f.path}`).join('\t'))
+    if (!await git.checkIsRepo(project.argv.root ? CheckRepoActions.IS_REPO_ROOT : CheckRepoActions.IN_TREE)) return false
+
+    const packageRoot = resolve(process.cwd(), name.slice(1)),
+      gitRoot = (await git.raw('rev-parse', '--git-dir')).trim().slice(0, -4) || packageRoot,
+      relRoot = relative(gitRoot, packageRoot)
+
+    const files = (await git.status()).files
+      .filter(f => isSubdirectoryOf(f.path, relRoot) && (f.path = relative(relRoot, f.path)))
+      .filter(f => !project.argv.workingDirectories || project.argv.workingDirectories.includes(f.working_dir))
+      .map(f => `${(yellow(f.working_dir))} ${f.path}`)
+    if (files.length) {
+      console.log(cyan(name))
+      console.log(files.join('\n'))
+    }
+    return true
   } catch (e) {
     console.log(red(name), e)
     return false
   }
-  return true
+}
+
+async function add(project: Project, name: string) {
+  try {
+    if (!name) return false
+    const git: SimpleGit = simpleGit(name.slice(1))
+    if (!await git.checkIsRepo(project.argv.root ? CheckRepoActions.IS_REPO_ROOT : CheckRepoActions.IN_TREE)) return false
+
+    await git.add('.')
+    return true
+  } catch (e) {
+    console.log(red(name), e)
+    return false
+  }
 }
 
 async function commit(project: Project, name: string) {
   try {
     if (!name) return false
     const git: SimpleGit = simpleGit(name.slice(1))
-    if (!await git.checkIsRepo(CheckRepoActions.IS_REPO_ROOT)
-    ) return false
-    console.log(await git.add('.').commit(project.argv.message))
+    if (!await git.checkIsRepo(project.argv.root ? CheckRepoActions.IS_REPO_ROOT : CheckRepoActions.IN_TREE)) return false
+
+    const res = await git.add('.').commit(project.argv.message)
+    return !!res.commit
   } catch (e) {
     console.log(red(name), e)
     return false
   }
-  return true
 }
 
 async function push(project: Project, name: string) {
   try {
     if (!name) return false
     const git: SimpleGit = simpleGit(name.slice(1))
-    if (!await git.checkIsRepo(CheckRepoActions.IS_REPO_ROOT)
-    ) return false
+    if (!await git.checkIsRepo(project.argv.root ? CheckRepoActions.IS_REPO_ROOT : CheckRepoActions.IN_TREE)) return false
+
     const s = await git.status()
     if (s.isClean()) await git.push(project.argv.remote, project.argv.branch)
+    return true
   } catch (e) {
     console.log(red(name), e)
     return false
   }
-  return true
 }
 
-async function choreBumpVersions(project: Project, name: string) {
+async function chore(project: Project, name: string) {
   try {
     if (!name) return false
     const git: SimpleGit = simpleGit(name.slice(1))
-    if (!await git.checkIsRepo(CheckRepoActions.IS_REPO_ROOT)
-    ) return false
+    if (!await git.checkIsRepo(project.argv.root ? CheckRepoActions.IS_REPO_ROOT : CheckRepoActions.IN_TREE)) return false
+
     const s = await git.status()
     const files = s.files
       .filter(f => ['M', ' '].includes(f.working_dir) && f.path.split('/').reverse()[0] === 'package.json')
@@ -70,9 +111,10 @@ async function choreBumpVersions(project: Project, name: string) {
 
 const subcommands: Record<string, (project: Project, name: string) => Promise<boolean>> = {
   status,
+  add,
   commit,
   push,
-  chore: choreBumpVersions,
+  chore,
 }
 
 register('git', async (project) => {
@@ -86,13 +128,15 @@ register('git', async (project) => {
     Object.keys(project.targets).map(name => action(project, name)),
   )).filter(x => x).length
 
-  console.log(green(`Successfully updated ${counter} repositories`))
+  console.log(green(`Successfully processed ${counter} repositories`))
 }, {
   alias: {
     dry: ['d'],
     message: ['m'],
     remote: ['r'],
     branch: ['b'],
+    workingDirectories: ['W'],
+    root: ['R'],
   },
   default: {
     message: '',
