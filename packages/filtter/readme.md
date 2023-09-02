1b9bc0823ed21bc5afedb37433ae9ccc14885d5b
Hieuzest <girkirin@hotmail.com>
2023-09-02 19:32:21 +0800

    feat(filter): add plugin filter

---
+++ packages/filtter/readme.md
@@ -0,0 +1,7 @@
+# yakumo-filter-targets
+
+[![downloads](https://img.shields.io/npm/dm/yakumo-filter?style=flat-square)](https://www.npmjs.com/package/yakumo-filter)
+[![npm](https://img.shields.io/npm/v/yakumo-filter?style=flat-square)](https://www.npmjs.com/package/yakumo-filter)
+[![GitHub](https://img.shields.io/github/license/Hieuzest/yakumo?style=flat-square)](https://github.com/Hieuzest/yakumo/blob/master/LICENSE)
+
+Filter located targets.
---
+++ packages/filtter/src/index.ts
@@ -0,0 +1,22 @@
+import { addHook } from 'yakumo'
+import { Eval, executeEval } from '@minatojs/core'
+import {} from 'yakumo-core-patch'
+
+declare module 'yakumo' {
+  export interface Commands {
+    [key: string]: FilterConfig
+  }
+}
+
+export interface FilterConfig {
+  'filter-targets'?: Eval.Expr<boolean>
+}
+
+addHook('execute.before', async (project, name) => {
+  console.log(project.argv)
+  const expr = project.config.commands?.[name]?.['filter-targets']
+  if (!expr) return
+  project.targets = Object.fromEntries(Object.entries(project.targets).filter(([path, json]) => {
+    return executeEval({ _: { path, ...json } }, expr)
+  }))
+})
