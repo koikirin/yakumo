26e94dae9e76315bc55c31915ecc713efd58f626
Hieuzest <girkirin@hotmail.com>
2025-12-20 02:08:35 +0800

    feat(core): fix color for yakumo run

---
+++ packages/core/src/plugins/run.ts
@@ -0,0 +1,24 @@
+import { Context, manager, spawnAsync } from '../index.js'
+
+export const inject = ['yakumo']
+
+export function apply(ctx: Context) {
+  ctx.register('run', async () => {
+    const { argv, cwd } = ctx.yakumo
+    const [command, ...rest] = argv['--'] as string[]
+    if (!command) throw new Error('Missing command')
+    const paths = ctx.yakumo.locate(argv._, {
+      filter: () => true,
+    }).filter(path => {
+      return !!ctx.yakumo.workspaces[path].scripts?.[command]
+    })
+    for (const path of paths) {
+      const agent = manager?.name || 'npm'
+      await spawnAsync([agent, 'run', command, ...rest], {
+        cwd: cwd + path,
+        stdio: 'inherit',
+        shell: true,
+      })
+    }
+  })
+}
